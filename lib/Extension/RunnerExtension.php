3e34ff338ad2fc89d5b1ac4b8d497094acf68ae1
Daniel Leech <dan.t.leech@gmail.com>
2021-11-28 10:47:24 +0100

    Add memory sampler

--- lib/Extension/RunnerExtension.php
+++ lib/Extension/RunnerExtension.php
@@ -41,6 +41,7 @@ use PhpBench\Executor\ScriptBuilder;
 use PhpBench\Executor\ScriptExecutor;
 use PhpBench\Executor\Unit\CallSubjectUnit;
 use PhpBench\Executor\Unit\HrtimeSampler;
+use PhpBench\Executor\Unit\MemorySampler;
 use PhpBench\Executor\Unit\RootUnit;
 use PhpBench\Executor\Unit\SetupUnit;
 use PhpBench\Expression\Evaluator;
@@ -51,6 +52,7 @@ use PhpBench\Json\JsonDecoder;
 use PhpBench\Model\MainResultFactory;
 use PhpBench\Model\Result\BufferResultFactory;
 use PhpBench\Model\Result\HrTimeResultFactory;
+use PhpBench\Model\Result\MemoryResultFactory;
 use PhpBench\Progress\Logger\BlinkenLogger;
 use PhpBench\Progress\Logger\DotsLogger;
 use PhpBench\Progress\Logger\HistogramLogger;
@@ -393,6 +395,7 @@ class RunnerExtension implements ExtensionInterface
                     new SetupUnit(),
                     new CallSubjectUnit(),
                     new HrtimeSampler(),
+                    new MemorySampler(),
                 ]),
                 new ScriptExecutor(
                     new PhpProcessFactory(
@@ -410,6 +413,7 @@ class RunnerExtension implements ExtensionInterface
                 new MainResultFactory([
                     'hrtime' => new HrTimeResultFactory(),
                     'buffer' => new BufferResultFactory(),
+                    'mem' => new MemoryResultFactory(),
                 ]),
             );
         }, [
